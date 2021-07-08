1f9005c0180683959c0a083fcf58169a9d1bcb6b
am3ra <jp2kk2@gmail.com>
2021-07-08 12:39:23 -0500

    aligned text, changed font, added icons, fix full circle when break, and color

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="container flex flex-center ">
+  <div class="container flex flex-center q-mt-lg">
     <div class="text-h1">
       {{status.label}}
     </div>
     <q-circular-progress
       :value="percentage"
       size="75vh"
-      color="primary"
-      class="q-pt-xl q-ml-xl"
+      :color="status.color"
+      class=" q-ml-xl"
       label="percentage"
       show-value
     >
     <p v-if="!loading" class="text-h3">
-      {{ status.totalTime - minutes + status.baseTime }} : {{ String(59 - seconds).padStart(2,'0') }}
+      {{ status.totalTime - 1 - minutes + status.baseTime }} : {{ String(59 - seconds).padStart(2,'0') }}
     </p>
     </q-circular-progress>
    <!-- {{ currentTime }} -->
@@ -60,7 +60,7 @@ export default defineComponent({
     }
 
     const percentage = computed(() =>
-      minutes.value < 45 ? minutes.value / 45 * 100 : minutes.value / 15 * 100
+      minutes.value < 45 ? minutes.value / 45 * 100 : (minutes.value - 45) / 15 * 100
     )
 
     setInterval(getNow, 1000)
@@ -75,3 +75,11 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss">
+
+.text-h1, p {
+  font-family: 'Interstate';
+}
+
+</style>
